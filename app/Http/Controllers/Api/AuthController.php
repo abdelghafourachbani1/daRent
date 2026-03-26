608602679d1f5318ced88c6bea504a2cdd6b1ebe
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-26 15:46:26 +0100

    add the login function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller {
     }
 
     public function register(Request $request) {
-        
+
         $data = $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
@@ -22,6 +22,26 @@ class AuthController extends Controller {
         ]);
         $user = $this->authService->register($data);
 
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request) {
+        $data = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = $this->authService->login($data);
+        if (!$user) {
+            return response()->json(
+                ['message' => 'Invalid credentials'],401
+            );
+        }
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
