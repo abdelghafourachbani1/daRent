0fdcd70b06374e8312cb0ad69ce59eaa4460e536
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-26 15:19:38 +0100

    add the register and login in the authservice

---
+++ app/Services/AuthService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Pest\Support\Arr;
+
+class AuthService {
+
+    public function register(array $data) {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' => $data['role'],
+        ]);
+    }
+
+    public function login(array $data) {
+        $user = User::where('email' , $data['email'])->first();
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return null;
+        }
+        return $user;
+    }
+
+}
