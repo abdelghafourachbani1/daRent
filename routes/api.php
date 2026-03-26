7ff603ac83b2d58365e7a1ad0ba43e9325847dc3
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-27 00:27:36 +0100

    add the propertie routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PropertyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,4 +16,14 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
     });
+});
+
+Route::get('/properties', [PropertyController::class, 'index']);
+Route::get('/properties/{id}', [PropertyController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/properties', [PropertyController::class, 'store']);
+    Route::put('/properties/{id}', [PropertyController::class, 'update']);
+    Route::delete('/properties/{id}', [PropertyController::class, 'destroy']);
+    Route::get('/my-properties', [PropertyController::class, 'myProperties']);
 });
