8458ddad3f7901983f44734e2ad487dc124ced4b
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-27 10:27:47 +0100

    owner can store a property

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -13,4 +13,27 @@ class PropertyController extends Controller
         return response()->json(Property::with('user')->latest()->get());
     }
 
+    public function store(Request $request) {
+        if ($request->user()->role !== 'owner') {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ],403);
+        }
+        $data = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'city' => 'required',
+            'adress' => 'required',
+            'type' => 'required'
+        ]);
+
+        $property = Property::create([
+            ...$data,
+            'user_id' => $request->user()->id()
+        ]);
+
+        return response()->json($property,201);
+    }
+
 }
