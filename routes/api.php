627113b76eb33f4b7efdc739649e5b084eac8857
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-27 09:48:48 +0100

    add the propertie routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PropertyController;
+<<<<<<< HEAD
+=======
+
+>>>>>>> fceda3a (add the propertie routes)
 
 Route::get('/user', function (Request $request) {
     return $request->user();
