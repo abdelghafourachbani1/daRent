b3293a29c9f293ee46b17becf188f0f5fe109e47
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-27 09:52:11 +0100

    add the index function to propertie controller

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    //
+
+    public function index() {
+        return response()->json(Property::with('user')->latest()->get());
+    }
+
 }
