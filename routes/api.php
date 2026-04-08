f6788483276bb52ab78d30c30ee37d11ec5dcc80
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-04-08 14:49:05 +0100

    update the api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PropertyController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register',[AuthController::class, 'register']);
@@ -15,4 +16,26 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/auth/me', [AuthController::class, 'updateProfile']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
+});
+
+Route::get('/properties', [PropertyController::class, 'index']);
+Route::get('/properties/{property}', [PropertyController::class, 'show']);
+ 
+Route::middleware('auth:sanctum')->group(function () {
+ 
+    Route::middleware('role:owner')->group(function () {
+        Route::post('/properties', [PropertyController::class, 'store']);
+ 
+        Route::put('/properties/{property}', [PropertyController::class, 'update']);
+ 
+        Route::delete('/properties/{property}', [PropertyController::class, 'destroy']);
+ 
+        Route::get('/my-properties', [PropertyController::class, 'myProperties']);
+ 
+        Route::patch('/properties/{property}/rent', [PropertyController::class, 'markAsRented']);
+ 
+        Route::get('/properties/{property}/stats', [PropertyController::class, 'stats']);
+ 
+        Route::post('/properties/{property}/images', [PropertyController::class, 'uploadImages']);
+    });
 });
