3d5541d88b7de54474c70512090a383bdb408eed
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-04-10 17:47:04 +0100

    add the reservations migrations

---
+++ database/migrations/2026_04_10_164435_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('property_id')->constrained('properties')->onDelete('cascade');
+            $table->foreignId('tenant_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->decimal('prix_total', 10, 2);
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->text('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
