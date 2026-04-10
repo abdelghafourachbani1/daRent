9eb34f5107988664a2a7700d6730ddcf865d741f
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-04-10 17:25:31 +0100

    update the channels.app

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\Conversation;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('conversation.{conversationId}', function ($user, $conversationId) {
+    $conversation = Conversation::find($conversationId);
+ 
+    if (!$conversation) {
+        return false;
+    }
+    return $conversation->isParticipant($user->id);
 });
