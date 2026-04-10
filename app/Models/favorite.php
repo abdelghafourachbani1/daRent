2b91e8eadd10b891d35dfaa82ad4e8f1154c6d53
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-04-10 02:36:08 +0100

    update the Favorite Model

--- app/Models/favorite.php
+++ app/Models/favorite.php
@@ -3,8 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class favorite extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'property_id',
+        'date_ajout',
+    ];
+
+    protected $casts = [
+        'date_ajout' => 'date',
+    ];
+
+    public static function getFavoritesByUser(int $userId) {
+        return static::where('user_id', $userId)
+                     ->with('property.city', 'property.media')
+                     ->get();
+    }
+
+    public static function clearAll(int $userId): void {
+        static::where('user_id', $userId)->delete();
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+ 
+    public function property() {
+        return $this->belongsTo(Property::class);
+    }
 }
